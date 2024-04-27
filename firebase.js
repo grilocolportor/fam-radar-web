95b78ae3a697a398a1db3b010402e50d83f37894
venancio <antoniovenancio10@gmail.com>
2024-04-27 14:45:22 -0300

    ajustes

--- firebase.js
+++ firebase.js
@@ -1,76 +1,30 @@
+ // Import the functions you need from the SDKs you need
 
-  // // // Import the functions you need from the SDKs you need
-  // import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
-  // import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-analytics.js";
-  // // TODO: Add SDKs for Firebase products that you want to use
-  // // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // // Your web app's Firebase configuration
-  // // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  // const firebaseConfig = {
-  //   apiKey: "AIzaSyCPTIAjkP6qFhqw1Y2JkO6YTqsM99uualA",
-  //   authDomain: "fam-radar.firebaseapp.com",
-  //   projectId: "fam-radar",
-  //   storageBucket: "fam-radar.appspot.com",
-  //   messagingSenderId: "644911681418",
-  //   appId: "1:644911681418:web:2e5dfccdc1bf7d8e5fc273",
-  //   measurementId: "G-LZJTPZLG8G"
-  // };
-
-  // // // Initialize Firebase
-  // const app = initializeApp(firebaseConfig);
-  // const analytics = getAnalytics(app);
-
-  // function getDados(){
-  //   const db = firebase.firestore();
-  //   return "";
-  // }
-
-  // module.exports = { getDados };
-
-// var moduloFuncoes = (function(){
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
+ import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-analytics.js";
+ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
+ import { collection, getDocs,  doc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js"; 
   
-//  function getdaddos(){
-//   const db = firebase.firestore();
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
 
-//   // Adicionar dados ao Firestore
-//   // db.collection("users").add({
-//   //   firstName: "John",
-//   //   lastName: "Doe"
-//   // })
-//   // .then((docRef) => {
-//   //   console.log("Documento escrito com ID: ", docRef.id);
-//   // })
-//   // .catch((error) => {
-//   //   console.error("Erro ao adicionar documento: ", error);
-//   // });
-  
-//   //<input type="checkbox" id="scrollZoom" checked="checked"></input>
-  
-//   db.collection("users").get().then((querySnapshot) => {
-//       const dados = [];
-//       querySnapshot.forEach((doc) => {
-//         dados.push({
-//           id: doc.id,
-//           nome: doc.data().nome,
-//           idade: doc.data().idade,
-//         });
-//       });
-//       const jsonDados = JSON.stringify(dados);
-//       console.log(jsonDados);
-//       // Utilize o jsonDados aqui
-  
-//       const itemElement = document.createElement("li");
-//       itemElement.innerHTML = `
-//         <h3>${item.nome}</h3>
-//         <p>Idade: ${item.idade}</p>
-//       `;
-//       itemElement.classList.add("item"); // Adicionar uma classe para estilização
-//       dataContainer.appendChild(itemElement);
-//     });
-// }
-// });
+ // Your web app's Firebase configuration
+ // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ const firebaseConfig = {
+   apiKey: "AIzaSyCPTIAjkP6qFhqw1Y2JkO6YTqsM99uualA",
+   authDomain: "fam-radar.firebaseapp.com",
+   projectId: "fam-radar",
+   storageBucket: "fam-radar.appspot.com",
+   messagingSenderId: "644911681418",
+   appId: "1:644911681418:web:2e5dfccdc1bf7d8e5fc273",
+   measurementId: "G-LZJTPZLG8G"
+ };
 
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+ const analytics = getAnalytics(app);
+ const db  = getFirestore(app);
 
- 
+ export {app, analytics, db,  collection, getDocs,  doc, onSnapshot}
+
 
